caa5d636474b13cb880fe6a7d58e471aa0319dc7
sugiartofahmi <sugiartofahmi@gmail.com>
2025-12-14 20:28:46 +0700

    feat: create category controller

--- app/Domain/Backoffice/Category/Messages/CategoryMessage.php
+++ app/Domain/Backoffice/Category/Messages/CategoryMessage.php
@@ -7,6 +7,8 @@ namespace App\Domain\Backoffice\Category\Messages;
 class CategoryMessage
 {
     public const NOT_FOUND = 'Category not found';
+    public const INDEX_SUCCESS = 'Success get categories';
+    public const SHOW_SUCCESS = 'Success get category detail';
     public const CREATE_SUCCESS = 'Category created successfully';
     public const UPDATE_SUCCESS = 'Category updated successfully';
     public const DELETE_SUCCESS = 'Category deleted successfully';
